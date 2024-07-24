2360a0a0e6d382dd98b8a997e4483689a5a486dc
Dev <chaithongsorawit61@gmail.com>
2024-07-24 13:47:43 +0700

    .

--- day1/ex01/lecture.c
+++ day1/ex01/lecture.c
@@ -4,6 +4,8 @@ int	main()
 {
 	int age = 20;
 	printf("I am %d years old.\n", age);
+	scanf("%d", &age);
+	printf("I am %d years old.\n", age);
 	//printf("Hello World");
 	return (0);
 }
---
+++ day1/ex02/lecture.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int	main()
+{
+	int a = 11;
+	int b = 5;
+
+	printf("Sum = %d\n", a+b);
+	printf("Sub = %d\n", a-b);
+	printf("Mul = %d\n", a*b);
+	printf("Div = %d\n", a/b);
+	printf("Mod = %d\n", a%b);
+}	
